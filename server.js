a42c568e6c2f5699cdf4d7083349a2b98822b882
Cicciostra <65898184+Cicciostra@users.noreply.github.com>
2026-03-02 16:17:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ passport.use(new SamlStrategy(
   {
     entryPoint: process.env.SAML_ENTRY_POINT,
     issuer: process.env.SAML_ISSUER,
-    cert: process.env.SAML_CERT
+    cert: process.env.SAML_CERT,
+    callbackUrl: process.env.BASE_URL + "/app1/acs", // default
+    protocol: "https://"
   },
   (profile, done) => done(null, profile)
 ));
@@ -49,3 +51,4 @@ app.post("/app1/acs",
  
 
 app.listen(process.env.PORT || 3000);
+
